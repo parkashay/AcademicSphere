aff453180374b5306f333529117cb4a10ddac8e1
Rita adhikari <ritaadhikari936@gmail.com>
2023-08-26 18:33:37 +0545

    feat/staff

---
+++ app/Filament/Resources/StaffResource/Pages/CreateStaff.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\StaffResource\Pages;
+
+use App\Filament\Resources\StaffResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStaff extends CreateRecord
+{
+    protected static string $resource = StaffResource::class;
+    protected function getRedirectUrl(): string
+    {
+     return $this->getResource()::getUrl('index');  
+    }
+}
