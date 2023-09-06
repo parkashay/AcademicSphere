b291f8acab2d8c37fbbe770b2b40eeab72d26ff1
Prakash Poudel <prakaas,poudell@gmail.com>
2023-09-06 12:28:54 +0545

    front

--- resources/views/components/news-preview.blade.php
+++ resources/views/components/news-preview.blade.php
@@ -7,8 +7,7 @@
                     <h2 class="mb-0 text-nowrap mr-3 section-heading highlight ml-0">Notice Board</h2>
                     <div class="border-top w-100 border-primary d-none d-sm-block"></div>
                     <div>
-                        <a href="/posts"
-                            class="btn btn-sm btn-primary-outline ml-sm-3 d-none d-sm-block outlined-btn">See
+                        <a href="/posts" class="btn btn-sm btn-primary-outline ml-sm-3 d-none d-sm-block outlined-btn">See
                             All</a>
                     </div>
                 </div>
@@ -17,34 +16,31 @@
         <!-- course list -->
         <div class="row justify-content-center mt-2">
             @foreach ($noticeBoard as $notice)
-                <!-- course item -->
-                <div class="col-lg-4 col-sm-6 mb-5">
-                    <div class="card p-0 border-primary rounded-0 hover-shadow">
-                        <div class="card-body">
-                            <ul class="list-inline mb-2">
-                                <li class="list-inline-item mr-3 home-post-date">
-                                    <?php
-                                    $dateTime = new DateTime(substr($notice->created_at, 0, 10));
-                                    $formattedDate = $dateTime->format('F j, Y');
-                                    print_r($formattedDate);
-                                    ?>
-                                </li>
-                                <li class="list-inline-item">
-                                    @foreach ($notice->category as $category)
-                                        <span class="category-badge">{{ $category }}</span>
-                                    @endforeach
-                                </li>
-                            </ul>
-                            <a href="/posts/single/{{ $notice->id }}">
-                                <h4 class="card-title">{{ $notice->title }}</h4>
-                            </a>
-                            <p class="card-text mb-4"> {!! Str::limit($notice->content, 30) !!}
-                            </p>
-                            <a href="/posts/single/{{ $notice->id }}" class="btn btn-primary btn-sm">Read More</a>
-                        </div>
+            <!-- course item -->
+            <div class="col-lg-4 col-sm-6 mb-2">
+                <div class="card p-0 rounded-0 hover-shadow">
+                    <div class="card-body">
+                        <ul class="list-inline mb-3 ">
+                            <li class="list-inline-item mr-3 home-post-date">
+                                <?php
+                                $dateTime = new DateTime(substr($notice->created_at, 0, 10));
+                                $formattedDate = $dateTime->format('F j, Y');
+                                print_r($formattedDate)
+                                ?>
+                            </li>
+                            <li class="list-inline-item">
+                                @foreach ($notice->category as $category)
+                                <span class="category-badge"><a href="/search?search={{$category}}">{{strtoupper($category)}}</a></span>
+                                @endforeach
+                            </li>
+                        </ul>
+                        <a href="/posts/single/{{$notice->id}}" class="home-page-post-title">
+                            {{$notice->title}}
+                        </a>
                     </div>
                 </div>
-                <!-- /course item -->
+            </div>
+            <!-- /course item -->
             @endforeach
         </div>
     </div>
